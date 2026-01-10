6824266937868d5c47ec90e85b9ae9df7fd57aaf
Jesse Ngolab <jessengolab.dev@gmail.com>
2026-01-10 05:24:19 -0600

    fixed scroll effect

---
+++ src/components/SmoothScroll.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
+    return (
+        <ReactLenis
+            root // <--- IMPORTANT: This must be here
+            options={{
+                lerp: 0.1,
+                duration: 1.5,
+                smoothWheel: true,
+            }}
+        >
+            {children as any}
+        </ReactLenis>
+    );
+}
