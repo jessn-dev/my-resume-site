6cde31ad39d8a63c3f75b547117aa20f82d54340
Jesse Ngolab <jessengolab.dev@gmail.com>
2026-01-24 04:42:10 -0600

    added projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Experience from '@/components/Experience';
+import Projects from "@/components/Projects";
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
@@ -11,6 +12,7 @@ export default function Home() {
             <Navbar />
             <Hero />
             <Experience />
+            <Projects />
             <About />
             <Contact />
             <Footer />
